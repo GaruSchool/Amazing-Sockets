b6775edac40a426372c96bd66c6c979bd71d1845
GaruGaru <garuglieri68@gmail.com>
2014-11-06 23:39:37 +0100

    Updated Test Class

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,18 +4,12 @@ import ServerSide.GaruServer;
  * Created by Garu on 06/11/2014.
  */
 public class MainClass {
-
-    public static void main(String[] args)
-    {
-       // new GaruServer("Garu",9999).startListening();
-
-
-        new Thread(){
+    public static void main(String[] args) {
+        new Thread() {
             @Override
             public void run() {
-                new GaruServer("Garu",9999).startListening();
+                new GaruServer("Garu", 9999).startListening();
             }
         }.start();
-
     }
 }
